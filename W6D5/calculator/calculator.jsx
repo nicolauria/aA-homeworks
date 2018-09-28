4e0f9d74b9b7607cea38f7be18da40998516b807
nico <nicolauria@outlook.com>
2018-09-28 08:48:47 -0700

    complete W6D5 homework

---
+++ W6D5/calculator/calculator.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Calculator extends React.Component{
+  constructor(props){
+    super(props);
+    
+  }
+
+
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+        //your code will replace this
+      </div>
+    );
+  }
+}
+
+export default Calculator;
